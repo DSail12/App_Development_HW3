389709e682f2791a076c67b7c154866dfcd785f1
dsail <dsail@yandex.ru>
2023-07-05 00:07:33 +0300

    Initial Commit

---
+++ HW3_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3_swift
+//
+//  Created by Eugene Zvyagin on 05.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
